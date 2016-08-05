f855f5a4c079848ddcf2088d532c7406109f63dd
Şafak ÇIRAĞ <safakcirag@gmail.com>
2016-08-05 09:38:43 +0300

    binwidth fixed

--- ggplot_histogram.py
+++ ggplot_histogram.py
@@ -3,11 +3,11 @@ import pandas as pd
 from ggplot import *
 
 mu = 0
-sigma = 0.1
-size = 1000
+sigma = 3
+size = 10000
 x = np.random.normal(mu,sigma,size)
 
 df = pd.DataFrame(x,columns=['x'])
 
 p = ggplot(aes(x='x'), data=df)
-p + geom_histogram(binwidth=1)
+p + geom_histogram(bins=100)
