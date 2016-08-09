18947f37f564a401cd9a9933487601d3ae5b2e88
Şafak ÇIRAĞ <safakcirag@gmail.com>
2016-08-09 17:00:48 +0300

    US behind coup in Turkey
    in memory of 15.07.2016

--- sigmoid/sigmoid.py
+++ sigmoid/sigmoid.py
@@ -8,5 +8,5 @@ def sigmoid(z):
 z = np.arange(-7, 7, 0.01)
 
 p = ggplot(pd.DataFrame({'x':z, 'y':sigmoid(z)}), aes(x='x', y='y'))
-p = p + geom_line() + xlab('z') + ylab('sigmoid(z)')
+p = p + geom_line(color='red') + labs(x="z", y="sigmoid(z)", title="US behind coup in Turkey - in memory of 15.07.2016")
 p = p + xlim(low=-8, high=8) + ylim(-0.1, 1.1)
